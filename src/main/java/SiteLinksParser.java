812d190ec2c85c35b06483ac2a1c40c9854ded4e
vladimirov <denis.vladimiroff@gmail.com>
2019-01-30 11:48:35 +0200

    alpha version

---
+++ src/main/java/SiteLinksParser.java
@@ -0,0 +1,39 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class SiteLinksParser {
+
+    public static void main(String[] args) throws IOException {
+
+        String url = "";
+        Elements links = getElements(url, "Total links: ");
+
+        for (Element link : links) {
+            String hrefUrl = link.attr("href");
+            if (!"#".equals(hrefUrl) && !hrefUrl.isEmpty()) {
+                System.out.println(hrefUrl);
+                Elements linksInternal = getElements(hrefUrl, "Total internal links : ");
+                for (Element linkInternal : linksInternal) {
+                    String hrefInternalUrl = linkInternal.attr("href");
+                    if (!"#".equals(hrefInternalUrl) && !hrefInternalUrl.isEmpty()) {
+                        System.out.println(hrefInternalUrl);
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    private static Elements getElements(String url, String s) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        System.out.println(s + links.size());
+        return links;
+    }
+
+}
